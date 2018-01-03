1462fba403835a0d1e0be384049ca3390b550da1
wangshichao <wangshichao@babyfs.cn>
2018-01-03 16:00:15 +0800

    增加guice provider

---
+++ src/main/java/com/babyfs/tk/galaxy/guice/ClientProxyBuilderModel.java
@@ -0,0 +1,35 @@
+package com.babyfs.tk.galaxy.guice;
+
+
+import com.babyfs.tk.galaxy.client.AbstractClientProxy;
+import com.babyfs.tk.galaxy.client.ClientProxyBuilder;
+import com.babyfs.tk.galaxy.client.RpcOkHttpClient;
+import com.babyfs.tk.galaxy.register.IDiscoveryProperties;
+import com.babyfs.tk.galaxy.register.LoadBalanceImpl;
+import com.google.inject.Inject;
+import com.google.inject.PrivateModule;
+import com.google.inject.Provider;
+
+public class ClientProxyBuilderModel extends PrivateModule {
+    @Override
+    protected void configure() {
+
+        bind(ClientProxyBuilder.class).toProvider(ClientProxyBuilderModel.ClientProxyBuilderProvider.class).asEagerSingleton();
+        expose(ClientProxyBuilder.class);
+    }
+
+    private static class ClientProxyBuilderProvider implements Provider<ClientProxyBuilder> {
+
+        @Inject
+        private RpcOkHttpClient rpcOkHttpClient;
+
+        @Inject
+        private IDiscoveryProperties discoveryProperties;
+
+        @Override
+        public ClientProxyBuilder get() {
+            LoadBalanceImpl loadBalance = LoadBalanceImpl.builder().discoveryProperties(discoveryProperties).build();
+            return AbstractClientProxy.builder().loadBalance(loadBalance).client(rpcOkHttpClient);
+        }
+    }
+}
---
+++ src/main/java/com/babyfs/tk/galaxy/guice/RpcOkHttpClientModel.java
@@ -0,0 +1,37 @@
+package com.babyfs.tk.galaxy.guice;
+
+import com.babyfs.tk.commons.config.IConfigService;
+import com.babyfs.tk.galaxy.client.RpcOkHttpClient;
+import com.google.inject.Inject;
+import com.google.inject.PrivateModule;
+import com.google.inject.Provider;
+
+public class RpcOkHttpClientModel extends PrivateModule {
+
+    private static final long CONNECT_TIMEOUT = 5;
+
+    private static final long READ_TIMEOUT = 5;
+
+    private static final long WRITE_TIMEOUT = 5;
+
+    @Override
+    protected void configure() {
+
+        bind(RpcOkHttpClient.class).toProvider(RpcOkHttpClientModel.OkHttpClientProvider.class).asEagerSingleton();
+        expose(RpcOkHttpClient.class);
+    }
+
+    private static class OkHttpClientProvider implements Provider<RpcOkHttpClient> {
+
+        @Inject(optional = true)
+        private IConfigService conf;
+
+        @Override
+        public RpcOkHttpClient get() {
+
+            RpcOkHttpClient rpcOkHttpClient = new RpcOkHttpClient();
+            rpcOkHttpClient.init(CONNECT_TIMEOUT,READ_TIMEOUT,WRITE_TIMEOUT);
+            return rpcOkHttpClient;
+        }
+    }
+}
