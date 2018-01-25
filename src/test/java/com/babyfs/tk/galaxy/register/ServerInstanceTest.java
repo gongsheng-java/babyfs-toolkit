832f101f285b8cced7fad4a51fedc0fccbef09f9
wangshichao <wangshichao@babyfs.cn>
2018-01-25 20:07:40 +0800

    采用有序号的临时节点，解决session失效有短时间负载均衡器失效的问题

---
+++ src/test/java/com/babyfs/tk/galaxy/register/ServerInstanceTest.java
@@ -0,0 +1,33 @@
+package com.babyfs.tk.galaxy.register;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class ServerInstanceTest {
+
+
+    private static  final Logger  LOGGER = LoggerFactory.getLogger(ServerInstanceTest.class);
+
+    @Test
+    public  void  deDoubleTest(){
+
+        ServiceInstance serviceInstanceA = new ServiceInstance("api","127.0.0.1",8080);
+        ServiceInstance serviceInstanceB = new ServiceInstance("api","127.0.0.1",8080);
+        List<ServiceInstance>  list = new CopyOnWriteArrayList<>();
+        if(!list.contains(serviceInstanceA)){
+            LOGGER.error(" no contains A");
+            list.add(serviceInstanceA);
+        }
+        if(!list.contains(serviceInstanceB)){
+            list.add(serviceInstanceA);
+        }else {
+            LOGGER.error("contains B");
+        }
+        Assert.assertTrue(list.size()==1);
+    }
+}
