d281953afedbde99ea516cc1084fe98e9e6d1a93
wangdongyong <wangdongyong@babyfs.cn>
2018-05-02 15:50:46 +0800

    去掉sleep

--- src/test/java/com/babyfs/tk/probe/metrics/JVMProcessUsageGaugeSetTest.java
+++ src/test/java/com/babyfs/tk/probe/metrics/JVMProcessUsageGaugeSetTest.java
@@ -2,17 +2,12 @@ package com.babyfs.tk.probe.metrics;
 
 import com.codahale.metrics.Gauge;
 import com.codahale.metrics.Metric;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Map;
 
-import static org.junit.Assert.*;
-
 public class JVMProcessUsageGaugeSetTest {
-
     @Test
-    @Ignore
     public void getMetrics() throws InterruptedException {
         JVMProcessUsageGaugeSet set = new JVMProcessUsageGaugeSet();
         for (int i = 0; i < 100; i++) {
@@ -21,9 +16,6 @@ public class JVMProcessUsageGaugeSetTest {
                 System.out.println(m.getKey() + "=" + ((Gauge) m.getValue()).getValue());
             }
         }
-
-        Thread.sleep(3000);
-
         for (int i = 0; i < 100; i++) {
             Map<String, Metric> metrics = set.getMetrics();
             for (Map.Entry<String, Metric> m : metrics.entrySet()) {
