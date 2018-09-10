c46b767963bd5849a713917930f3258b8e011ecd
zhanghongyun <zhanghongyun@babyfs.cn>
2018-09-10 12:29:28 +0800

    add methodname

--- src/main/java/com/babyfs/tk/service/biz/web/backend/ServiceMethodAspect.java
+++ src/main/java/com/babyfs/tk/service/biz/web/backend/ServiceMethodAspect.java
@@ -5,12 +5,14 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.aspectj.lang.reflect.CodeSignature;
 
+import java.lang.reflect.Method;
 import java.util.List;
 
 /**
@@ -26,16 +28,33 @@ public class ServiceMethodAspect {
 
     @Around(value = "execution(public * com.babyfs..*.*Impl.*(..))")
     public Object MethodAccess(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-        JoinPoint.StaticPart thisJoinPointStaticPart = proceedingJoinPoint.getStaticPart();
-        Object[] paramValues = proceedingJoinPoint.getArgs();
-        String[] paramNames = ((CodeSignature) thisJoinPointStaticPart
-                .getSignature()).getParameterNames();
-        StringBuilder logLine = new StringBuilder(thisJoinPointStaticPart
-                .getSignature().getName()).append("(");
-        if (paramNames.length != 0)
-            logParamValues(logLine, paramNames, paramValues);
-        logLine.append(") - finished");
-        return proceedingJoinPoint.proceed();
+
+        Object returnValue = proceedingJoinPoint.proceed();
+
+        try {
+//            MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+//            Method method = methodSignature.getMethod();
+//            String methodName = method.getName();
+//            String className = method.getDeclaringClass().getSimpleName();
+
+            JoinPoint.StaticPart thisJoinPointStaticPart = proceedingJoinPoint.getStaticPart();
+            Object[] paramValues = proceedingJoinPoint.getArgs();
+            String[] paramNames = ((CodeSignature) thisJoinPointStaticPart
+                    .getSignature()).getParameterNames();
+            StringBuilder logLine = new StringBuilder(thisJoinPointStaticPart
+                    .getSignature().getName()).append("(");
+            if (paramNames.length != 0)
+                logParamValues(logLine, paramNames, paramValues);
+            logLine.append(") - finished");
+            logLine.append(" result:").append(JSONObject.toJSON(returnValue));
+
+            _logger.info(logLine.toString());
+        }
+        catch (Exception ex)
+        {
+            _logger.error("ServiceMethodAspect记录方法ERROR",ex);
+        }
+        return returnValue;
     }
 
     private static void logParamValues(StringBuilder logLine,
