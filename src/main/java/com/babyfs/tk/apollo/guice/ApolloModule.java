2602bd1aa5d8dbbcbd35ec727980d20dbfa150da
wangkun <wangkun@babyfs.cn>
2018-10-08 10:43:06 +0800

    apollo应用

---
+++ src/main/java/com/babyfs/tk/apollo/guice/ApolloModule.java
@@ -0,0 +1,10 @@
+package com.babyfs.tk.apollo.guice;
+
+import com.google.inject.AbstractModule;
+
+public class ApolloModule  extends AbstractModule {
+    @Override
+    protected void configure() {
+        
+    }
+}
