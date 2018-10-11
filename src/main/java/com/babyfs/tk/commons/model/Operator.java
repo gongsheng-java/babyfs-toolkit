2da77e2d9f54202050931dfddeb02741f3bffef9
huyihuan <huyihuan@babyfs.cn>
2018-10-11 21:55:52 +0800

    添加机器人接口定义

---
+++ src/main/java/com/babyfs/tk/commons/model/Operator.java
@@ -0,0 +1,43 @@
+package com.babyfs.tk.commons.model;
+
+/*
+* 请求操作人信息
+* */
+public class Operator {
+    private String ip;
+    private String name;
+    private String source;
+    private String token;
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
---
+++ src/main/java/com/babyfs/tk/commons/model/ServiceRequest.java
@@ -0,0 +1,16 @@
+package com.babyfs.tk.commons.model;
+
+/*
+* 请求基类
+* */
+public class ServiceRequest {
+    private Operator operator;
+
+    public Operator getOperator() {
+        return operator;
+    }
+
+    public void setOperator(Operator operator) {
+        this.operator = operator;
+    }
+}
