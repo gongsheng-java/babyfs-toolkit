8b630efd85ec7b2bb885c7aaec8f5a154e84123d
huyihuan <huyihuan@babyfs.cn>
2018-10-12 11:19:06 +0800

    新增机器人对接相关接口定义

--- src/main/java/com/babyfs/tk/commons/model/ServiceRequest.java
+++ src/main/java/com/babyfs/tk/commons/model/ServiceRequest.java
@@ -6,6 +6,10 @@ package com.babyfs.tk.commons.model;
 public class ServiceRequest {
     private Operator operator;
 
+    public ServiceRequest() {
+        //todo:需要增加operator初始化机制，从web底层穿透到service层
+    }
+
     public Operator getOperator() {
         return operator;
     }
