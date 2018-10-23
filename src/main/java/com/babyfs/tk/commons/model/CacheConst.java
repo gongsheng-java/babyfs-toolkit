fd2f8b9a152f42d903135b73d076e20b5a1bfd41
huyihuan <huyihuan@babyfs.cn>
2018-10-23 15:41:45 +0800

    新增operator记录以及暂时去掉激活码3次限制

---
+++ src/main/java/com/babyfs/tk/commons/model/CacheConst.java
@@ -0,0 +1,14 @@
+package com.babyfs.tk.commons.model;
+
+/**
+ * @program BabyFs
+ * @description: 缓存公用常量
+ * @author: huyihuan
+ * @create: 2018/10/23
+ */
+public class CacheConst {
+    /*
+    * 当前请求操作人
+    * */
+    public static final String REQUEST_OPERATER = "request_context_operator";
+}
